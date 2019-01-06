d0f4f1ba03033d334cfba36d35806f3bf26be341
rosewhitt <39914401+rosewhitt@users.noreply.github.com>
2019-01-05 21:12:32 -0500

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var ages = [4, 12, 16, 20];
-
-function checkAdult(age) {
-    return age >= document.getElementById("ageToCheck").value;
-}
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = ages.find(checkAdult);
-}
